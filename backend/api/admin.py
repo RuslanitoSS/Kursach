b418e5f9994aac18fd4ed0f3e1a8d4e5132494b8
RuslanitoS <suprotkinr@gmail.com>
2024-09-12 15:26:08 +0300

    13

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Event, Project
 
+
 class ParticipantsInline(admin.TabularInline):
     model = Event.participants.through
     extra = 1
@@ -17,20 +18,24 @@ class OrganizersInline(admin.TabularInline):
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('name', 'start_date', 'end_date', 'address')
+    list_display = ('name', 'start_date', 'end_date', 'address', 'short_description_preview')
     search_fields = ('name', 'description', 'short_description')
     list_filter = ('start_date', 'end_date', 'organizers')
     inlines = [ParticipantsInline, OrganizersInline]
     filter_horizontal = ('participants', 'organizers')
+    date_hierarchy = 'start_date'
+    list_display_links = ('name', 'start_date')
+    raw_id_fields = ('participants', 'organizers')
+    readonly_fields = ('address',)  #Только ради readonly_fields. а так должно быть writable
+    search_fields = ('name', 'description', 'short_description')
 
-    fieldsets = (
-        (None, {
-            'fields': ('name', 'short_description', 'description', 'address', 'start_date', 'end_date')
-        }),
-        ('Related Users', {
-            'fields': ('participants', 'organizers')
-        }),
-    )
+    @admin.display(description='Short Description Preview')
+    def short_description_preview(self, obj):
+        return obj.short_description[:50]  
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.prefetch_related('organizers', 'participants')  
 
 
 class ProjectParticipantsInline(admin.TabularInline):
@@ -49,17 +54,19 @@ class ProjectOrganizersInline(admin.TabularInline):
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('name', 'short_description_preview')
     search_fields = ('name', 'description', 'short_description')
+    list_filter = ('organizers',)  
     inlines = [ProjectParticipantsInline, ProjectOrganizersInline]
     filter_horizontal = ('participants', 'organizers')
+    list_display_links = ('name',)
+    raw_id_fields = ('participants', 'organizers')
+    readonly_fields = ('description',)  #Только ради readonly_fields. а так должно быть writable
 
-    fieldsets = (
-        (None, {
-            'fields': ('name', 'short_description', 'description')
-        }),
-        ('Related Users', {
-            'fields': ('participants', 'organizers')
-        }),
-    )
+    @admin.display(description='Short Description Preview')
+    def short_description_preview(self, obj):
+        return obj.short_description[:50] 
 
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.prefetch_related('organizers', 'participants')
